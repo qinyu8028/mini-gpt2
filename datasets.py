ed0db635cf877b2389d5d154a5cde20912e13649
qinyu8028 <2537946012@qq.com>
2026-05-15 23:14:41 +0800

    add DPO datasets

--- datasets.py
+++ datasets.py
@@ -197,3 +197,84 @@ class HeldOutSonnetsDataset(Dataset):
     }
 
     return batched_data
+
+
+class DPOSonnetDataset(Dataset):
+  def __init__(self, positive_path, negative_path):
+    self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+    self.tokenizer.pad_token = self.tokenizer.eos_token
+    self.sonnets_pairs = self._load_sonnets(positive_path, negative_path)
+
+  def _load_sonnets(self, positive_path, negative_path):
+    """Reads the file and extracts individual held out sonnets(first 3 lines)"""
+    with open(positive_path, 'r', encoding='utf-8') as f:
+      positive_text = f.read()
+    with open(negative_path, 'r', encoding='utf-8') as f:
+      negative_text = f.read()
+
+    # Split sonnets based on numbering pattern (e.g., "\n\n1\n\n")
+    positive_sonnets = re.split(r'\n\s*(\d+)\s*\n', positive_text)[1:]  # Remove header text
+    negative_sonnets = re.split(r'\n\s*(\d+)\s*\n', negative_text)[1:]
+    # negative_sonnets = re.findall(r'\n\n(\d+)([\s\S]+?)\n\n', negative_text)
+
+    # The negative result might have some fake ids in between the lines, e.g., "0730"
+    # Thus, must make sure our ids are valid
+    negative_numbers = negative_sonnets[0::2]
+    negative_contents = negative_sonnets[1::2]
+
+    negative_result = {}
+    last_valid_num = 0
+    for num_str, content in zip(negative_numbers, negative_contents):
+        num = int(num_str)
+        if num == last_valid_num + 1:
+            negative_result[num] = content
+            last_valid_num = num
+        else:
+            negative_result[last_valid_num] = negative_result[last_valid_num] + '\n' + num_str + '\n' + content
+        
+    positive_numbers = positive_sonnets[0::2]
+    positive_contents = positive_sonnets[1::2]
+    positive_result = {int(num_str): content for num_str, content in zip(positive_numbers, positive_contents)}
+
+    sonnets_pairs = []
+    for key in positive_result.keys() & negative_result.keys():
+      x = positive_result[key].strip().split('\n')[:3]  # extract only the first three lines
+      y_win = positive_result[key].strip().split('\n')[3:]
+      y_lose = negative_result[key].strip().split('\n')[3:]
+      sonnets_pairs.append((x, y_lose, y_win))
+
+    return sonnets_pairs
+
+  def __len__(self):
+    return len(self.sonnets_pairs)
+
+  def __getitem__(self, idx):
+    return self.sonnets_pairs[idx]
+
+  def collate_fn(self, sonnets_pairs):
+    x = ['\n'.join(example[0]) for example in sonnets_pairs]
+    negative = ['\n'.join(example[0] + example[1]) for example in sonnets_pairs]
+    positive = ['\n'.join(example[0] + example[2]) for example in sonnets_pairs]
+
+    negative_encoding = self.tokenizer(negative, return_tensors='pt', padding=True, truncation=True)
+    positive_encoding = self.tokenizer(positive, return_tensors='pt', padding=True, truncation=True)
+    x_encoding = self.tokenizer(x, return_tensors='pt', padding=True, truncation=True)
+
+    negative_token_ids = torch.LongTensor(negative_encoding['input_ids'])
+    negative_attention_mask = torch.LongTensor(negative_encoding['attention_mask'])
+
+    positive_token_ids = torch.LongTensor(positive_encoding['input_ids'])
+    positive_attention_mask = torch.LongTensor(positive_encoding['attention_mask'])
+
+    x_len = x_encoding['attention_mask'].sum(dim=1)
+
+    batched_data = {
+      'negative_token_ids': negative_token_ids,
+      'negative_attention_mask': negative_attention_mask,
+      'positive_token_ids': positive_token_ids,
+      'positive_attention_mask': positive_attention_mask,
+      'x_len': x_len
+    }
+
+    return batched_data
